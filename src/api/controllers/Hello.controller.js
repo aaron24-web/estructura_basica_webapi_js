034f1873456dc8e20a5277989f7b97088af0bb3e
aaron24-web <Aarontunwork24@gmail.com>
2025-07-08 10:41:42 -0600

    v2 de estructura

--- src/api/controllers/Hello.controller.js
+++ src/api/controllers/Hello.controller.js
@@ -1,6 +1,6 @@
 // Esta es la función que maneja la petición
 const getHelloWorld = (req, res) => {
-  res.status(200).json({ message: '¡Hola Mundo desde una estructura organizada!' });
+  res.status(200).json({ message: '¡Hola Mundo!' });
 };
 
 // Exportamos la función para poder usarla en otro archivo
